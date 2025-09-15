4e2e45f9577069a74426706bb24963caa6c6d20e
Md. Masud <82032856+masud1680@users.noreply.github.com>
2025-09-15 12:32:11 +0600

    Create event_numbers.dart

---
+++ event_numbers.dart
@@ -0,0 +1,20 @@
+
+
+void main(){
+
+  
+// print 1 to   50 event numbers
+  
+  for(int i = 1; i <= 50; i++){
+    
+    if(i % 2 == 0){
+      
+          print(i);
+      
+    }
+
+  }
+  
+  
+  
+}
