8cb79c0c55d0ff98407c8f0460967616a15f6e8e
Md. Masud <82032856+masud1680@users.noreply.github.com>
2025-09-15 12:37:32 +0600

    Create find_nagative_positive_zero.dart

---
+++ find_nagative_positive_zero.dart
@@ -0,0 +1,22 @@
+
+
+void main(){
+
+  
+// print 1 to   50 event numbers
+  
+  for(int i = -10; i <= 5; i++){
+    
+    if(i < 0){
+          print("$i is Negative.");
+    }else if(i == 0){
+      print("$i is Zero.");
+    }else if(i > 0){
+      print("$i is Positive.");
+    }
+
+  }
+  
+  
+  
+}
