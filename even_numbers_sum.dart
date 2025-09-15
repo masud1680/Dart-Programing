ea0811644856a691548954033db6a4e7f74559f8
Md. Masud <82032856+masud1680@users.noreply.github.com>
2025-09-15 12:43:08 +0600

    Create even_numbers_sum.dart

---
+++ even_numbers_sum.dart
@@ -0,0 +1,39 @@
+
+
+void main(){
+  
+
+  
+  int evenSum = 0;
+  
+  for(int i = 1; i <= 100; i++){
+    
+    if(i % 2 == 0){
+     
+            evenSum += i;
+    }
+
+      
+  }
+  
+  print("Result is : $evenSum");
+  
+  
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
