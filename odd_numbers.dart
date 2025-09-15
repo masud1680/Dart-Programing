c645f4633fbf0fda642de3081819067d5d15fb17
Md. Masud <82032856+masud1680@users.noreply.github.com>
2025-09-15 12:33:18 +0600

    Create odd_numbers.dart

---
+++ odd_numbers.dart
@@ -0,0 +1,19 @@
+
+void main(){
+
+  
+// print 1 to   50 odd numbers
+  
+  for(int i = 1; i <= 50; i++){
+    
+    if(i % 2 != 0){
+      
+          print(i);
+      
+    }
+
+  }
+  
+  
+  
+}
