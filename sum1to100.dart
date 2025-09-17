3ed42ed19ce8a84bdc52e7bda157e51065fe5283
Md. Masud <82032856+masud1680@users.noreply.github.com>
2025-09-17 23:35:42 +0600

    Create sum1to100.dart

---
+++ sum1to100.dart
@@ -0,0 +1,20 @@
+
+void main(){
+
+  
+// print sum 1 to   100 numbers
+  
+sum();
+  
+}
+
+void sum(){
+  int sum = 0;
+  
+  for(int i = 1; i <= 100; i++){
+      sum += i;
+      
+  }
+  
+  print("Result is : $sum");
+}
