d4ccfb1ae0fc1e23a06c004bf3538ec296e87936
Md. Masud <82032856+masud1680@users.noreply.github.com>
2025-09-17 23:23:55 +0600

    Create print_even_number.dart

---
+++ print_even_number.dart
@@ -0,0 +1,25 @@
+void main(){
+  
+  
+// print 1 to   50 event numbers
+  evenNumber();
+
+}
+
+void evenNumber(){
+    String output = "";
+  
+  for(int i = 1; i <= 50; i++){
+    
+    if(i % 2 == 0){
+      
+    output += "$i ";
+      
+    }
+    
+
+  }
+  
+  print(output);
+}
+  
